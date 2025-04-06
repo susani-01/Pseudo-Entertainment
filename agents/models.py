5796ce09e7c94e82c736cedcb3f973f58c0ac99e
qjrm1430 <qjrm1430@naver.com>
2025-04-06 23:55:34 +0900

    add: .env.example, fix models

--- agents/models.py
+++ agents/models.py
@@ -1,21 +1,11 @@
-from langchain_cohere import CohereRerank
 from langchain_openai import ChatOpenAI
-from langchain_upstage import UpstageEmbeddings
 
 
 class Model:
-    @staticmethod
-    def get_embedding_model():
-        return UpstageEmbeddings(model="embedding-query")
-
     @staticmethod
     def get_openai_single_model(temperature=0.1):
         return ChatOpenAI(model="gpt-4o-mini", temperature=temperature)
 
     @staticmethod
     def get_openai_multi_model(temperature=0.1):
-        return ChatOpenAI(model="gpt-4o-mini", temperature=temperature)
-
-    @staticmethod
-    def get_rerank_model():
-        return CohereRerank(model="rerank-multilingual-v3.0")
+        return ChatOpenAI(model="gpt-4o", temperature=temperature)
